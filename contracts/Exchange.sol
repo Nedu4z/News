773a81c0ad062d40c70f8e3dfdb2d2cb1833a38e
Nedu4z <paschalamadi604@gmail.com>
2025-02-12 17:45:46 +0100

    11. Cancel order

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(
@@ -26,7 +27,15 @@ contract Exchange {
         address tokenGive,
         uint256 amountGive,
         uint256 timestamp
-
+    );
+    event Cancel( // Here's the missing event declaration
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
     );
     struct _Order {
             //Attributes of an order
@@ -34,7 +43,7 @@ contract Exchange {
             address user;//User who made order
             address tokenGet; //Address of the token they receieve
             uint256 amountGet; //Amount they receieve
-            address tokenGive; //Address of the token they receive
+            address tokenGive; //Address of the token they give
             uint256 amountGive; //Amount they recieve
             uint256 timestamp;//when order was created
     }
@@ -109,10 +118,32 @@ contract Exchange {
             block.timestamp
         );
     }
+    function cancelOrder(uint256 _id) public {
+        // Fetch order
+        _Order storage _order = orders[_id];
+
+        //Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+
+        // Order must exist
+        require(_order.id == _id);
+
+        // Cancel the order
+        orderCancelled[_id] = true;
+
+        
+
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
  
 }
-
-
-
-
-
